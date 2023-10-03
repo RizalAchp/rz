ff87edbf81db888fd55292e247b0ee0521877c14
RizalAchp <rizalachmad00@gmail.com>
2023-10-04 04:13:48 +0700

    initial commit

---
+++ cb.c
@@ -0,0 +1,21 @@
+
+#define CB_IMPLEMENTATION
+#include "cb.h"
+
+// todo
+cb_status_t build(cb_t *cb) {
+    (void)cb;
+    return CB_OK;
+}
+// todo
+cb_status_t tests(cb_t *cb) {
+    (void)cb;
+    return CB_OK;
+}
+
+int main(int argc, char *argv[]) {
+
+    CB_GO_REBUILD_URSELF(argc, argv);
+
+    return EXIT_SUCCESS;
+}
